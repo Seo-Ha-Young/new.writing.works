7a6a3902dc703db1de4a7a78ea81103a54f7f1af
shy <tjgkdud054@gmail.com>
2023-11-21 15:05:45 +0900

    마리아DB설정 및 연결 완료, DB에 간단한 Eassy 등록 및 확인 성공

--- src/main/java/writing/board/entity/BaseEntity.java
+++ src/main/java/writing/board/entity/BaseEntity.java
@@ -1,4 +1,23 @@
 package writing.board.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = { AuditingEntityListener.class})
+@Getter
 public class BaseEntity {
+    @CreatedDate
+    @Column(name = "regDate", updatable = false)
+    private LocalDateTime regDate;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long no;
 }
