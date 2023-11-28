a90a5d84ed070220acc9d780bf3b45302f9f8601
Seo-Ha-Young <144644741+Seo-Ha-Young@users.noreply.github.com>
2023-11-28 17:46:57 +0900

    게시판 게시글 보이도록 설정중..

--- src/main/java/writing/board/controller/BoardController.java
+++ src/main/java/writing/board/controller/BoardController.java
@@ -3,14 +3,22 @@ package writing.board.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import writing.board.dto.PageRequestDTO;
+import writing.board.service.BoardService;
 
 @Controller
 @RequestMapping("/html")
 @RequiredArgsConstructor
 @Log4j2
 public class BoardController {
+private final BoardService boardService;
     @GetMapping("/board")
-    public void board() {}
+    public void board(PageRequestDTO requestDTO, Model model) {
+        log.info("board page................"+requestDTO);
+        model.addAttribute("result", boardService.getList(requestDTO));
+    }
+
 }
