f3d0af75c10a36e734cb6f3f6bd0d7d4d4b9f302
Zzangshinyeong <144649517+Zzangshinyeong@users.noreply.github.com>
2023-12-02 03:11:53 +0900

    write.html에서 js로 자꾸 안넘어가서 이거 해결 중입니다ㅠㅠ.. 다시 수정하겠습니다..

---
+++ src/main/java/writing/board/controller/PostWriteController.java
@@ -0,0 +1,37 @@
+package writing.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import writing.board.dto.PageRequestDTO;
+import writing.board.dto.PostWrittenDTO;
+import writing.board.service.BoardService;
+import writing.board.service.PostService;
+
+@Controller
+@RequiredArgsConstructor
+@Log4j2
+public class PostWriteController {
+    private final PostService postService;
+
+    // 글 작성 페이지로 이동
+    @GetMapping("/html/write")
+    public void write(PageRequestDTO requestDTO, Model model) {
+        log.info("board page................"+requestDTO);
+        //model.addAttribute("result", boardService.getList(requestDTO));
+    }
+
+
+    // 글 작성 처리
+    @PostMapping("/savePost")
+    public String savePost(@ModelAttribute PostWrittenDTO writeDTO) {
+        log.info("글 작성 요청: " + writeDTO);
+       // postService.savePost(); // BoardService를 통해 글 작성 로직 수행
+        return "redirect:/html/board"; // 작성 후 목록 페이지로 리다이렉트
+    }
+}
