161d6862a518769d9b28e0c02aed72d6e4697cad
Zzangshinyeong <144649517+Zzangshinyeong@users.noreply.github.com>
2023-12-05 17:43:08 +0900

    글쓰기 페이지 불러오기 저장하기 기능 완료

--- src/main/java/writing/board/service/PostService.java
+++ src/main/java/writing/board/service/PostService.java
@@ -2,7 +2,11 @@ package writing.board.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import writing.board.dto.PostWrittenDTO;
+import writing.board.entity.PostWritten;
 import writing.board.repository.PostWrittenRepository;
+
+import java.util.List;
 
 @Service
 public class PostService {
@@ -14,16 +18,13 @@ public class PostService {
     }
 
     public void savePost(PostWrittenDTO postWrittenDTO) {
-        // PostWrittenDTO를 Post 엔티티로 변환하여 저장하는 예시
-        Post post = new Post();
-        post.setContent(postWrittenDTO.getContent());
-        // 작성자, 날짜 등의 정보도 필요한 경우에 설정
-
-        postRepository.save(post);
+        PostWritten postWritten = new PostWritten();
+        postWrittenRepository.getPostWithAll();
+        postWrittenRepository.save(postWritten);
     }
 
-    public List<Post> getAllPosts() {
-        return postRepository.findAll();
+    public List<PostWritten> getAllPosts() {
+        return postWrittenRepository.findAll();
     }
 
 }
