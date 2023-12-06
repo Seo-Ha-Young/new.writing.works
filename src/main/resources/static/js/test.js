ee3fff33003e749b2723eb6163fbcf52a619fd75
jjy <jjy8364@gmail.com>
2023-12-06 14:18:18 +0900

    12-06 푸터생성

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -3,8 +3,8 @@ let selectMode = "";
 let selectWord = "";
 let uploadBtn = "";
 const $write_warp = document.getElementById("write_warp");
-const $wirte_mode1 = document.getElementById("wirte_mode1");
-const $wirte_mode2 = document.getElementById("wirte_mode2");
+const $wirte_mode1 = document.getElementById("write_mode1");
+const $wirte_mode2 = document.getElementById("write_mode2");
 const $upload = document.getElementById("upload");
 const $upload2 = document.getElementById("upload2");
 const $mod = document.querySelector("#mod");
@@ -12,8 +12,8 @@ const $timer = document.querySelector("#timer");
 const $write_word = document.getElementById("write_word");
 const $write_word2 = document.getElementById("write_word2");
 let $boarder_wrap = document.getElementById("#board_wrap");
-let wirte_mod1 = document.querySelector("#timers");
-let wirte_mod2 = document.querySelector("#timers2");
+let write_mod1 = document.querySelector("#timers");
+let write_mod2 = document.querySelector("#timers2");
 
 let timer = $timer.value; //기준시간 작성
 
@@ -181,12 +181,12 @@ function buttonClick() {
   } else if (selectedValue === "1") {
     startTimer($timer.value);
     fadeOutElement("#board_wrap");
-    fadeInElement("#wirte_mode1");
+    fadeInElement("#write_mode1");
     fadeOutElement("#write_warp");
   } else if (selectedValue === "2") {
     startTimer($timer.value);
     fadeOutElement("#board_wrap");
-    fadeInElement("#wirte_mode2");
+    fadeInElement("#write_mode2");
     fadeOutElement("#write_warp");
     $write_word2.value = getRandomSentence();
   }
@@ -194,31 +194,31 @@ function buttonClick() {
 
 function savePost() {
   // 데이터를 가져오거나 생성합니다 (예: 제목, 내용 등)
-  const post_content = document.getElementById('write_word').value;
+  const post_content = document.getElementById("write_word").value;
   alert(title);
 
   // 서버로 전송할 데이터 객체를 생성합니다
   // Ajax를 이용해 서버에 데이터를 전송합니다
-  fetch('/savePost', {
-    method: 'POST',
+  fetch("/savePost", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(post_content)
+    body: JSON.stringify(post_content),
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    // 서버로부터 받은 응답에 대한 처리를 수행합니다
-    console.log('글이 성공적으로 저장되었습니다:', data);
-    // 저장 후 필요한 작업을 수행합니다 (예: 화면 갱신, 메시지 표시 등)
-  })
-  .catch(error => {
-    // 오류 발생 시 처리합니다
-    console.error('글을 저장하는 중 오류가 발생했습니다:', error);
-  });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // 서버로부터 받은 응답에 대한 처리를 수행합니다
+      console.log("글이 성공적으로 저장되었습니다:", data);
+      // 저장 후 필요한 작업을 수행합니다 (예: 화면 갱신, 메시지 표시 등)
+    })
+    .catch((error) => {
+      // 오류 발생 시 처리합니다
+      console.error("글을 저장하는 중 오류가 발생했습니다:", error);
+    });
 }
