59a7ec02e127935a650938d0a711d3bbd62b4663
Zzangshinyeong <144649517+Zzangshinyeong@users.noreply.github.com>
2023-12-15 02:40:46 +0900

    페이지 오류나서 일단 여기에 올려봅니다..

--- src/main/resources/static/js/write.js
+++ src/main/resources/static/js/write.js
@@ -1,4 +1,20 @@
-
+//function openTitlePopup() {
+// // 새로운 팝업을 현재 창의 중앙에 위치시키고 크기를 지정합니다.
+//  const popupWidth = 600;
+//  const popupHeight = 400;
+//  const left = (window.screen.width - popupWidth) / 2;
+//  const top = (window.screen.height - popupHeight) / 2;
+//  const popupURL = 'popup.html'; // 팝업으로 열 HTML 파일 경로
+//
+//  // 팝업 창 열기
+//  const popupWindow = window.open(popupURL, 'TitlePopup', `width=${popupWidth}, height=${popupHeight}, left=${left}, top=${top}`);
+//
+//  // 팝업 차단을 우회하기 위해 팝업이 정상적으로 열렸는지 확인합니다.
+//  if (!popupWindow || popupWindow.closed || typeof popupWindow.closed === 'undefined') {
+//    alert('팝업이 차단되었습니다. 팝업 차단을 해제해주세요.');
+//  }
+//
+//}
 function savePost() {
 
   const postContent = document.getElementById('write_word').value;
